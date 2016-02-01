1edfd9f57f4b4d5369a2c9468c5252d9fabb29bf
Rowan Abraham <rowan.abraham@gmail.com>
2016-01-31 17:43:54 -0800

    Improved versatility
    Improved the firmware to make it a bit more versatile (eg, only four
    bytes need to be sent to fill the entire string, five bytes can be sent
    to recolor a single pixel), added support for these capabilities to the
    library.

--- fastLED.py
+++ fastLED.py
@@ -7,21 +7,27 @@ num_leds = 60
 
 class fastLED:
 	def __init__(self):
-		self.ser = serial.Serial('COM4', 115200)
+		self.ser = serial.Serial('COM4', 57600, timeout = 1)
 		self.leds = [0] * (num_leds * 3)
 		atexit.register(self.close)
 
 	def write(self):
-		self.ser.write(bytes(self.leds))
+		self.ser.write(bytes([65] + self.leds))
+		sleep(0.005)
 
-	def put(self, color, pixel):
-		self.leds[pixel * 3] = int(color.red * 256)
-		self.leds[pixel * 3 + 1] = int(color.green * 256)
-		self.leds[pixel * 3 + 2] = int(color.blue * 256)
+	def put(self, color, pixel, write=True):
+		self.leds[pixel * 3] = int(color.red * 255)
+		self.leds[pixel * 3 + 1] = int(color.green * 255)
+		self.leds[pixel * 3 + 2] = int(color.blue * 255)
+		if write:
+			self.ser.write(bytes([80, pixel, int(color.red * 255), int(color.green * 255), int(color.blue * 255)]))
+			sleep(0.005)
 
-	def fill(self, color):
+	def fill(self, color, write=True):
 		self.leds = [int(color.red * 255), int(color.green * 255), int(color.blue * 255)] * num_leds
-		self.write()
+		if write:
+			self.ser.write(bytes([70, int(color.red * 255), int(color.green * 255), int(color.blue * 255)]))
+			sleep(0.005)
 
 	def close(self):
 		self.fill(Color('black'))
@@ -29,16 +35,40 @@ class fastLED:
 		self.ser.close()
 
 a = fastLED()
-a.fill(Color('black'))
+sleep(2)
+a.fill(Color('blue'))
+sleep(1)
+a.fill(Color('red'))
+sleep(1)
+a.fill(Color('blue'))
+sleep(1)
+a.fill(Color('purple'))
+sleep(1)
+# a.put(Color('red'), 3)
+# a.put(Color('blue'), 4)
+# a.put(Color('green'), 5)
+# sleep(2)
+# a.fill(Color('maroon'))
+# sleep(10)
 # a.put(Color('purple'), 3)
 # a.write()
 # sleep(1)
 # a.put(Color('black'), 3)
 # a.write()
 # sleep(1)
+# while True:
+# 	for i in list(Color('red').range_to(Color('green'), 50)):
+# 		sleep(.1)
+# 		a.fill(i)
+# 	for i in list(Color('green').range_to(Color('red'), 50)):
+# 		sleep(.1)
+# 		a.fill(i)
 while True:
-	for i in list(Color('black').range_to(Color('white'), 50)):
-		a.fill(i)
-	for i in list(Color('white').range_to(Color('black'), 50)):
-		a.fill(i)
-a.close()
+	c = Color('purple')
+	b = Color('black')
+	for i in range(0,59):
+		a.put(b, i)
+		if i is 0:
+			a.put(b, 59)
+		a.put(c, i+1)
+		sleep(0.1)
--- firmware/firmware.ino
+++ firmware/firmware.ino
@@ -1,6 +1,6 @@
 #include <FastLED.h>
 #define NUM_LEDS 60
-#define DATA_PIN 6
+#define DATA_PIN 5
 
 CRGB leds[NUM_LEDS];
 int incomingByte = 0;
@@ -8,19 +8,34 @@ byte buff[NUM_LEDS * 3];
 
 void setup() {
   FastLED.addLeds<NEOPIXEL, DATA_PIN>(leds, NUM_LEDS);
-  Serial.begin(115200);
+  Serial.begin(57600);
 }
 
 void loop() {
   if (Serial.available() > 0) {
-    Serial.readBytes(buff, NUM_LEDS * 3);
-    for(int led = 0; led < NUM_LEDS; led++){
-      leds[led].r = buff[3*led];
-      leds[led].g = buff[3*led+1];
-      leds[led].b = buff[3*led+2];
+    incomingByte = Serial.read(); /* first byte is the target method */
+    if(incomingByte == 'A'){ /* write *every* pixel from an array */
+      Serial.readBytes(buff, NUM_LEDS * 3);
+      for(int led = 0; led < NUM_LEDS; led++){
+        leds[led].setRGB(buff[3*led],buff[3*led+1],buff[3*led+2]);
+      }
+    }
+    else if(incomingByte == 'F'){ /* fill with single color
+                                     byte 0-2: rgb color */
+      Serial.readBytes(buff, 3);
+      for(int led = 0; led < NUM_LEDS; led++){
+        leds[led].setRGB(buff[0],buff[1],buff[2]);
+      }
+    }
+    else if(incomingByte == 'P'){ /* set single pixel
+                                     byte 0: target pixel
+                                     byte 1-3: rgb color */
+      Serial.readBytes(buff, 4);
+      leds[buff[0]].setRGB(buff[1],buff[2],buff[3]);
+    }
+    else{
+      
     }
     FastLED.show();
-    delay(10);
   }
-  delay(5);
 }
