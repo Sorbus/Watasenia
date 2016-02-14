78f7f3c96810a39af432a78c6f493eb7aa423691
Rowan Abraham <rowan.abraham@gmail.com>
2016-02-14 01:11:12 -0800

    Cleanup
    Cleaned up the snippets I was using for testing into a bunch of methods
    which send various patterns to the lights.

---
+++ snippets.py
@@ -0,0 +1,65 @@
+from colour import Color
+from time import sleep
+from modules.breathe import Breathe
+import datetime
+
+def rgbSolid(ledString, wait):
+	patSolid(ledString, [Color('red'),Color('green'), Color('blue')], wait)
+
+def patSolid(ledString, colorArray, wait):
+	for color in colorArray:
+		ledString.fill(color)
+		sleep(wait)
+
+def rgbFill(ledString, wait):
+	patFill(ledString, [Color('red'),Color('green'), Color('blue')], wait)
+
+def patFill(ledString, colors, wait):
+	for n in range(len(colors)):
+		for i in range(int(ledString.num_leds / len(colors))):
+			for b in range(len(colors)):
+				ledString.put(colors[b], i*len(colors) + ((n+b) if (n+b) < len(colors) else (n-(len(colors)-b))), False)
+		ledString.write()
+		sleep(wait)
+
+def something(ledString):
+	pos = 0
+	for i in list(Color('violet').range_to(Color('red'),30)):
+		ledString.put(i, pos, False)
+		pos += 1
+	ledString.write()
+
+def colorFade(ledString, start, end, steps, wait):
+	for i in list(start.range_to(end, steps)):
+		ledString.fill(i)
+		sleep(wait)
+	for i in list(end.range_to(start, steps)):
+		ledString.fill(i)
+		sleep(wait)
+
+def rainbowRotate(ledString, wait):
+	rainbow = list(Color('violet').range_to(Color('red'),30))
+	for i in range(0,29):
+		for n in range(0,30):
+			temp = (n+i)%29 if (n+i)>29 else (n+i)
+			ledString.put(rainbow[temp], n, False)
+		ledString.write()
+		sleep(wait)
+
+def travel(ledString, color, wait):
+	b = Color('black')
+	for i in range(0, ledString.num_leds - 1):
+		ledString.put(b, i)
+		if i is 0:
+			ledString.put(b, ledString.num_leds - 1)
+		ledString.put(color, i+1)
+		sleep(wait)
+
+def breathe(ledString, color):
+	b = Breathe()
+	c = color
+
+	while True:
+		c.luminance = (b.move()*0.5)
+		ledString.fill(c)
+		sleep(0.05)
