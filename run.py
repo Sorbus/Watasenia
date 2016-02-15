8d2ee13f8de5ccab6416f681f596bfa214d1d302
Rowan Abraham <rowan.abraham@gmail.com>
2016-02-15 00:37:01 -0800

    Added a bit more structure
    I'll eventually break out all of the relevant options into command line
    flags, but this is good as a proof of concept.

--- run.py
+++ run.py
@@ -1,22 +1,44 @@
 from time import sleep
 from seriaLED import seriaLED
 from colour import Color
+from multiprocessing import Process, Queue, Pipe
 import snippets
 
-ledString = seriaLED(30)
+def setup(q):
+	ledString = seriaLED(30)
 
-# snippets.rgbSolid(ledString, 1)
+	response = q.recv()
 
-# snippets.rgbFill(ledString, 1)
+	if response == 1:
+		snippets.breathe(ledString, Color('blue'))
+	elif response == 2:
+		while True:
+			snippets.rainbowRotate(ledString, 0.1)
+	elif response == 3:
+		while True:
+			snippets.colorFade(ledString, Color('violet'), Color('red'), 50, .1)
+	elif response == 4:
+		while True:
+			snippets.rgbFill(ledString, 1)
+	else:
+		quit()
 
-# snippets.patFill(ledString, [Color('orange'),Color('red'),Color('purple'),Color('green')], 1)
+if __name__ == '__main__':
+	q = Queue()
+	parent_conn, child_conn = Pipe(False)
+	p = Process(target=setup, args=(parent_conn,))
+	p.start()
 
-# snippets.colorFade(ledString, Color('orange'), Color('maroon'), 50, .1)
-
-# snippets.rainbowRotate(ledString, 0.1)
-
-# snippets.patSolid(ledString, [Color('red'),Color('green'), Color('blue')], 1)
-
-# snippets.travel(ledString, Color('red'), 0.5)
-
-snippets.breathe(ledString, Color('blue'))
+	print("\
+	#########################\n\
+	# Watasenia version 0.1 #\n\
+	#     by  Aucuparia     #\n\
+	#########################\n\
+	")
+	print("Options:\n\
+	1: Gentle Breathing\n\
+	2: Swirling Rainbow\n\
+	3: Fading Rainbow\n\
+	4: Rotating Pattern")
+	response = int(input("> "))
+	child_conn.send(response)
